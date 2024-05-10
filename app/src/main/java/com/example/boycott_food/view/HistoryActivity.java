809b42c6efec48cde2208979de7089e0f543cc9c
essra_hmida <hmidaesraa@gmail.com>
2024-05-10 11:31:46 +0100

    add history

---
+++ app/src/main/java/com/example/boycott_food/view/HistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.boycott_food.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.boycott_food.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
